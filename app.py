238f983ceb52600a6c656f5a79e6dfec0e8552bf
Carlos Gamero <cgamero@unique-yanbal.com>
2023-06-18 11:48:45 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+openai.api_key = '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'
+
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature = 0.5,
+    )
+    message = completions.choices[0].text
+    return message
+
+st.title('chatbot')
+
+# Storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+# We will get the user's input by calling the get_text function
+def get_text():
+    input_text = st.text_input("Tu: ","Hola, como estás?", key="input")
+    return input_text
+
+user_input = get_text()
+
+if user_input:
+    output = generate_response(user_input)
+    # store the output 
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+    
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
